9b24d8bd0220cc70d41f198ffe9702c6fdc15768
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2018-03-05 14:13:24 -0800

    Update mail_configuration.php

--- smart_railway_backend/mail_configuration.php
+++ smart_railway_backend/mail_configuration.php
@@ -3,7 +3,7 @@ define("PROJECT_HOME","http://localhost/phpsamples/");
 
 define("PORT", "587"); // port number
 define("MAIL_USERNAME", "railbeat.secuity@gmail.com"); // smtp usernmae
-define("MAIL_PASSWORD", "RailBeatak"); // smtp password
+define("MAIL_PASSWORD", "******"); // smtp password
 define("MAIL_HOST", "smtp.gmail.com"); // smtp host
 define("MAILER", "smtp");
 
